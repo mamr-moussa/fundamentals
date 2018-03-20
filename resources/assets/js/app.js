0af9df00dd2dbc43dd677b22cf01a6f296ecedcb
Amr <muhammad.amro.moussa@gmail.com>
2018-03-20 20:06:03 +0200

    20 mar 18 (1)

---
+++ resources/assets/js/app.js
@@ -0,0 +1,10 @@
+require('vue');   //Front End Framework.
+require('materialize-css'); //Front/JS/CSS Framework based on JS/CSS.
+window.Vue = require('vue');      //Front End Framework.
+
+window.App = new Vue({
+	el : '#app' , 
+	data : {
+		
+	}
+})
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel = "stylesheet" href = "{{asset('css/app.css')}}"/>
+    <title></title>
+</head>
+<body>
+    <div id = "app"></div>
+
+</body>
+<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+</html>
